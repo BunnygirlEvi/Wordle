929acc9b5ea62bab454a213a865d343439395fff
Evelin <evelin.b@Evelins-Air.lan>
2022-03-12 16:59:20 -0800

    add enter and delete

--- main.js
+++ main.js
@@ -4,50 +4,78 @@ const results = ["green", "gray", "yellow", "yellow", "green"];
 
 const wrapper = document.getElementById("app");
 
+const theWord = "clean";
+
 const wordle = ["", "", "", "", "", ""];
 
-function render(name) {
-  const item = name.toUpperCase();
+function render(char, colors) {
+  const item = char.toUpperCase();
   const line = document.createElement("div");
   line.className = "wordle-line";
   wrapper.appendChild(line);
   for (let i = 0; i < 5; i++) {
     const letterDiv = document.createElement("div");
-    if (results[i] == "green") letterDiv.className = "square green";
-    else if (results[i] == "yellow") {
+    if (colors[i] == "green") letterDiv.className = "square green";
+    else if (colors[i] == "yellow") {
       letterDiv.className = "square yellow";
-    } else if (results[i] == "gray") {
+    } else if (colors[i] == "gray") {
       letterDiv.className = "square gray";
+    } else {
+      letterDiv.className = "square";
     }
-    letterDiv.id = "square1";
     thing = item[i] || "";
     letterDiv.innerHTML = thing;
     line.appendChild(letterDiv);
   }
 }
 
-function draw(value) {
+function draw(value, result = []) {
   wrapper.innerHTML = "";
-  value.forEach(function (item) {
-    render(item);
+  value.forEach(function (item, index) {
+    render(item, index === activeLine ? result[index] || [] : []);
   });
 }
 
 let activeLine = 0;
 
 function onEnter(value) {
-  // wordle[0] += value;
-  wordle[activeLine] += value;
-  draw(wordle);
+  // if wordle is less than 5 then append
+  if (wordle[activeLine].length < 5) {
+    wordle[activeLine] += value;
+    draw(wordle);
+    console.log(wordle);
+  }
 }
 
 draw(wordle);
 document.addEventListener("keyup", function (event) {
+  // enter new line
   if (event.keyCode == 13) {
+    const result = validate(wordle[activeLine]);
+    if (!result) {
+      return;
+    }
     activeLine++;
+    draw(wordle, result);
+  } else if (event.keyCode >= 65 && event.keyCode <= 90) {
+    onEnter(event.key);
+  } else if (event.keyCode === 8) {
+    wordle[activeLine] = wordle[activeLine].slice(0, -1);
+    draw(wordle);
   }
 });
+
 createKeyboard();
+
+function validate(word) {
+  console.log("validating " + word);
+  // return false if length of the argument is not five
+  if (word.length < 5) {
+    console.log("less than 5. not accepted");
+    return false;
+  }
+  return ["green", "gray", "yellow", "yellow", "green"];
+}
 
 function createKeyboard() {
   const lines = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
